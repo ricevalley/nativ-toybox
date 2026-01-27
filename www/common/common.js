f46f535190b68f089c51118a34978955c194582b
ricevalley <dkome10sai@outlook.jp>
2026-01-27 23:08:52 +0900

    Add files via upload

--- www/common/common.js
+++ www/common/common.js
@@ -1,3 +1,7 @@
+//↓ fontawesome_kit_path
+const fontawesome_kit_path = '';
+if (!fontawesome_kit_path) console.error('Edit \'/common/common.js\' and import fontawesome.');
+
 class Header extends HTMLElement {
 	connectedCallback() {
 		this.innerHTML = `
@@ -30,17 +34,23 @@ window.addEventListener('DOMContentLoaded', () => {
 	customElements.define('set-header', Header);
 	customElements.define('set-footer', Footer);
 
+	//head
 	const common_head = `
-	<title>${typeof commonset_page_title !== 'undefined' ? commonset_page_title : 'not-set | Not-set'}</title>
-	<link rel="icon" type="image/png" sizes="48x48" href="/src/icons/icon-48.png">
-	<link rel="icon" type="image/png" sizes="72x72" href="/src/icons/icon-72.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="/src/icons/icon-96.png">
-	<link rel="icon" type="image/png" sizes="128x128" href="/src/icons/icon-128.png">
-	<link rel="icon" type="image/png" sizes="192x192" href="/src/icons/icon-192.png">
-	<link rel="icon" type="image/png" sizes="256x256" href="/src/icons/icon-256.png">
-	<link rel="icon" type="image/png" sizes="512x512" href="/src/icons/icon-512.png">
-	<link rel="manifest" href="/manifest.json">
+		<title>${typeof commonset_page_title !== 'undefined' ? commonset_page_title : 'not-set | Not-set'}</title>
+		<link rel="icon" type="image/png" sizes="48x48" href="/src/icons/icon-48.png">
+		<link rel="icon" type="image/png" sizes="72x72" href="/src/icons/icon-72.png">
+		<link rel="icon" type="image/png" sizes="96x96" href="/src/icons/icon-96.png">
+		<link rel="icon" type="image/png" sizes="128x128" href="/src/icons/icon-128.png">
+		<link rel="icon" type="image/png" sizes="192x192" href="/src/icons/icon-192.png">
+		<link rel="icon" type="image/png" sizes="256x256" href="/src/icons/icon-256.png">
+		<link rel="icon" type="image/png" sizes="512x512" href="/src/icons/icon-512.png">
+		<link rel="manifest" href="/manifest.json">
 	`;
-
 	document.head.insertAdjacentHTML('beforeend', common_head);
+
+	//fontawesome
+	const fontawesome_import = document.createElement('script');
+	fontawesome_import.src = fontawesome_kit_path;
+	fontawesome_import.crossOrigin = 'anonymous';
+	document.head.appendChild(fontawesome_import);
 });
